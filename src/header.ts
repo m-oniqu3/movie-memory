d5d04a27b065688888170537e3bb63d06dfb5b31
Monique <skaiwilliams85@gmail.com>
2023-03-05 01:29:36 -0500

    added dark logo to sign up page

--- src/header.ts
+++ src/header.ts
@@ -1,5 +1,5 @@
+import DarkLogo from "./assets/dark_logo.png";
 import Logo from "./assets/logo.png";
-
 export class BaseNav {
   generateNavbar() {
     const nav = document.querySelector(".nav") as HTMLElement;
@@ -12,5 +12,43 @@ export class BaseNav {
         `;
 
     return nav;
+  }
+}
+
+export class DarkBaseNav extends BaseNav {
+  windowWidth: number;
+
+  constructor() {
+    super();
+    this.windowWidth = 0;
+  }
+
+  getWindowWidth() {
+    this.windowWidth = window.innerWidth;
+  }
+
+  // override
+  generateNavbar() {
+    const nav = document.querySelector(".nav") as HTMLElement;
+    nav.classList.add("nav");
+    const imageSrc = this.windowWidth > 768 ? Logo : DarkLogo;
+
+    nav.innerHTML = `
+      <figure class="nav__logo">
+        <img src=${imageSrc} alt="logo" />
+        </figure>
+        `;
+
+    return nav;
+  }
+
+  generateNavOnResize() {
+    const generateNavAndWidth = () => {
+      this.getWindowWidth();
+      this.generateNavbar();
+    };
+
+    window.addEventListener("load", generateNavAndWidth);
+    window.addEventListener("resize", generateNavAndWidth);
   }
 }
---
+++ src/sign_up.ts
@@ -0,0 +1,5 @@
+import { DarkBaseNav } from "./header";
+
+// generate navbar with dark logo
+const baseNav = new DarkBaseNav();
+baseNav.generateNavOnResize();
