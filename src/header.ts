7ce77bb090990568db7022000522d5f056af4e2e
Monique <skaiwilliams85@gmail.com>
2023-03-05 00:04:14 -0500

    switched css to sass

--- src/header.ts
+++ src/header.ts
@@ -1,14 +1,12 @@
 import Logo from "./assets/logo.png";
 
 export class BaseNav {
-  constructor() {}
-
   generateNavbar() {
     const nav = document.querySelector(".nav") as HTMLElement;
 
-    nav.classList.add("header");
+    nav.classList.add("nav");
     nav.innerHTML = `
-      <figure class="header__logo">
+      <figure class="nav__logo">
         <img src=${Logo} alt="logo" />
         </figure>
         `;
