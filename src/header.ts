cc3d9a0302c57258ba43e9f91c95a1d92a938ba9
Monique <skaiwilliams85@gmail.com>
2023-03-10 20:34:19 -0500

    toggled active class on the nav awhen menu icon is clicked

--- src/header.ts
+++ src/header.ts
@@ -1,17 +1,83 @@
 import DarkLogo from "./assets/dark_logo.png";
 import Logo from "./assets/logo.png";
-export class BaseNav {
-  generateNavbar() {
-    const nav = document.querySelector(".nav") as HTMLElement;
 
-    nav.classList.add("nav");
-    nav.innerHTML = `
+import MenuIcon from "./assets/menu_icon.svg";
+import SearchIcon from "./assets/search_icon.svg";
+import UserIcon from "./assets/user_icon.svg";
+export class BaseNav {
+  nav: HTMLElement;
+  navContainer: HTMLElement;
+
+  constructor() {
+    this.nav = document.querySelector(".nav ") as HTMLElement;
+    this.navContainer = document.querySelector(
+      ".nav .container"
+    ) as HTMLElement;
+  }
+
+  generateNavbar() {
+    this.nav.classList.add("nav");
+    this.nav.innerHTML = `
       <figure class="nav__logo">
         <img src=${Logo} alt="logo" />
         </figure>
         `;
 
-    return nav;
+    return this.nav;
+  }
+
+  showNavLink() {}
+}
+
+export class FullNav extends BaseNav {
+  constructor() {
+    super();
+  }
+  showNavLink(): void {
+    console.log("show nav link");
+
+    const navLinks = document.querySelector(".nav__links") as HTMLElement;
+    navLinks.classList.toggle("nav__links--active");
+    this.nav.classList.toggle("nav--active");
+  }
+  generateNavbar() {
+    this.navContainer.classList.add("nav");
+    this.navContainer.innerHTML = `
+      <figure class="nav__logo">
+        <img src=${Logo} alt="logo" />
+        </figure>
+
+        <ul class="nav__links">
+          <li class="nav__item">
+            <a href="#" class="nav__link">Home</a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">About</a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">Contact</a>
+          </li>
+        </ul>
+
+
+        <div class="nav__icons">
+         <figure class="nav__icon">
+            <img src=${SearchIcon} alt="search icon" />
+          </figure>
+
+           <figure class="nav__icon">
+            <img src=${UserIcon} alt="user icon" />
+          </figure>
+
+          <figure class="nav__icon" id="menu">
+            <img src=${MenuIcon} alt="menu icon" />
+          </figure>
+
+         
+        </div>
+        `;
+
+    return this.nav;
   }
 }
 
@@ -29,17 +95,16 @@ export class DarkBaseNav extends BaseNav {
 
   // override
   generateNavbar() {
-    const nav = document.querySelector(".nav") as HTMLElement;
-    nav.classList.add("nav");
+    this.nav.classList.add("nav");
     const imageSrc = this.windowWidth > 900 ? Logo : DarkLogo;
 
-    nav.innerHTML = `
+    this.nav.innerHTML = `
       <figure class="nav__logo">
         <img src=${imageSrc} alt="logo" />
         </figure>
         `;
 
-    return nav;
+    return this.nav;
   }
 
   generateNavOnResize() {
