3b05611255117a6c80815678d37ae9410b6dc90a
Monique <skaiwilliams85@gmail.com>
2023-03-11 12:22:28 -0500

    showed text based on active page

--- src/home.ts
+++ src/home.ts
@@ -1,10 +1,23 @@
 import { FullNav } from "./nav/FullNav";
+import { Page } from "./pages/Page";
 
 const nav = new FullNav();
 nav.generateNavbar();
 
 const menu = document.getElementById("menu") as HTMLElement;
+const navLinks = document.querySelectorAll(".nav__container__link");
 
 menu.addEventListener("click", () => {
   nav.showNavLink();
+});
+
+const page = new Page();
+page.showContentForActivePage();
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const value = e.target as HTMLAnchorElement;
+    page.setActivePage(value.text);
+    page.showContentForActivePage();
+  });
 });
