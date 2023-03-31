287feaba3d61db9b733e50ad031dbeb838e4cb11
Monique <skaiwilliams85@gmail.com>
2023-03-30 20:42:12 -0500

    made footer responsive and added links

--- src/classes/Footer.ts
+++ src/classes/Footer.ts
@@ -1,3 +1,6 @@
+import GithubIcon from "../assets/github-icon.svg";
+import TwitterIcon from "../assets/twitter-icon.svg";
+
 export class Footer {
   footer: HTMLElement;
   constructor(element: HTMLElement) {
@@ -6,20 +9,30 @@ export class Footer {
 
   public generateFooter() {
     this.footer.innerHTML = `
-        
-      
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="browse.html">Home</a></li>
                     <li><a href="#">Memories</a></li>
-                    <li><a href="#">Tv Shows</a></li>
-                    <li><a href="#">Movies</a></li>
+                    <li><a href="tvshows.html">Tv Shows</a></li>
+                    <li><a href="movies.html">Movies</a></li>
                 </ul>
 
                 <p class="text"> 
                     Designed and Developed by Monique
                 </p>
-        
-       
+
+                <div class="icons">
+                    <a href="https://github.com/m-oniqu3">
+                    <figure>
+                        <img src=${GithubIcon} alt="github link to m-oniqu3">
+                    </figure>
+                    </a>
+                    
+                    <a href="https://twitter.com/ctrlaltmonique">
+                    <figure>
+                        <img src=${TwitterIcon} alt="twitter link to ctrlaltmonique">
+                    </figure>
+                    </a>
+                </div>
         `;
   }
 }
