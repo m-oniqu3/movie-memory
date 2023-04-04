f5342497e48a021792a0ca3c432eaaa548926726
Monique <skaiwilliams85@gmail.com>
2023-04-03 20:40:43 -0500

    added loading screen to movies page

--- src/classes/PopularMovies.ts
+++ src/classes/PopularMovies.ts
@@ -6,15 +6,32 @@ export class PopularMovies extends Movies {
   }
 
   private async getPopularMovies() {
-    const article = document.createElement("article");
-    const heading = this.generateHeading("Popular Movies");
+    let article = document.createElement("article");
+    let heading: HTMLHeadingElement;
+    //this.generateHeading("Popular Movies");
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&language=en-US&page=1`;
+
+    this.container.innerHTML = "";
+    const placeholder = this.populateLoadingPlaceholder("movies__placeholder");
 
     try {
       const popularMovies = await this.fetchMovies(url, "popularMovies");
-      const movieGrid = this.generateMovieGrid(popularMovies, "movie");
 
-      article.append(heading, movieGrid);
+      if (popularMovies.length === 0) {
+        heading = this.generateHeading("No Popular Movies");
+
+        article.innerHTML = "";
+        article = heading;
+        this.clearPlaceholderElement(placeholder);
+      } else {
+        heading = this.generateHeading("Popular Movies");
+        const movieGrid = this.generateMovieGrid(popularMovies, "movie");
+
+        article.innerHTML = "";
+        article.append(heading, movieGrid);
+        this.clearPlaceholderElement(placeholder);
+      }
+
       this.container.append(article);
     } catch (error) {
       console.log(error);
